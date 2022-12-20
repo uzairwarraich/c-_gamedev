5d34e985fac5d56814063d0ac55a1ed6eea8cc2a
“Uzairwarraich” <sunnyuzair91@gmail.com>
2022-12-20 16:12:46 -0500

    Initial setup for SDL and created a basic empty window

--- ArcadeApp/src/ArcadeApp.cpp
+++ ArcadeApp/src/ArcadeApp.cpp
@@ -7,9 +7,54 @@
 //============================================================================
 
 #include <iostream>
+#include <SDL2/SDL.h>
 using namespace std;
 
+
+const int SCREEN_WIDTH= 224;
+const int  SCREEN_HEIGHT = 288;
 int main() {
-	cout << "!!!Hello SDL!!!" << endl; // prints !!!Hello World!!!
-	return 0;
+
+	if (SDL_Init(SDL_INIT_VIDEO)){
+
+
+		std::cout << "Error SDL_Init Failed" <<std::endl;
+		return 1;
+	}
+
+	SDL_Window *optrWindow = SDL_CreateWindow("Arcade", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREEN_HEIGHT, 0);
+
+
+	if (optrWindow == nullptr){
+
+		std::cout << "Could not create window, got error : " << SDL_GetError() << std::endl;
+		return 1;
+	}
+
+
+	SDL_Event sdlEvent;
+
+	bool running = true;
+
+	while(running){
+
+		while( SDL_PollEvent(&sdlEvent)){
+
+			switch(sdlEvent.type){
+			case SDL_QUIT:
+				running = false;
+				break;
+			}
+		}
+	}
+
+
+	SDL_DestroyWindow(optrWindow);
+	SDL_Quit();
+
+
+
+
+
+		return 0;
 }
