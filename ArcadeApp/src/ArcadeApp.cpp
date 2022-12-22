296cba93cba1f6073e3d78b27b1d7fc0025b1ba7
“Uzairwarraich” <sunnyuzair91@gmail.com>
2022-12-22 13:55:37 -0500

    displaying a single red pixel at the center

--- ArcadeApp/src/ArcadeApp.cpp
+++ ArcadeApp/src/ArcadeApp.cpp
@@ -10,9 +10,15 @@
 #include <SDL2/SDL.h>
 using namespace std;
 
+void SetPixel(SDL_Surface * noptrWindowSurface, uint32_t colot , int x ,int y);
+size_t GetIndex(SDL_Surface * noptrSurface, int r , int c);
+
+
 
 const int SCREEN_WIDTH= 224;
 const int  SCREEN_HEIGHT = 288;
+
+
 int main() {
 
 	if (SDL_Init(SDL_INIT_VIDEO)){
@@ -31,6 +37,16 @@ int main() {
 		return 1;
 	}
 
+	SDL_Surface* noptrWindowSurface = SDL_GetWindowSurface(optrWindow);
+
+	SDL_PixelFormat* pixelFormat = noptrWindowSurface->format;
+
+	std::cout << "The window pixel format is : " << SDL_GetPixelFormatName(pixelFormat->format);
+
+	uint32_t color = 0xFF0000;
+
+	SetPixel(noptrWindowSurface, color, SCREEN_WIDTH/2, SCREEN_HEIGHT/2);
+	SDL_UpdateWindowSurface(optrWindow);
 
 	SDL_Event sdlEvent;
 
@@ -58,3 +74,25 @@ int main() {
 
 		return 0;
 }
+
+
+void SetPixel(SDL_Surface * noptrWindowSurface, uint32_t color , int x ,int y){
+
+	SDL_LockSurface(noptrWindowSurface);
+
+
+	uint32_t * pixels = (uint32_t*)noptrWindowSurface->pixels;
+
+	size_t index = GetIndex(noptrWindowSurface, y, x);
+
+	pixels[index] = color;
+
+
+	SDL_UnlockSurface(noptrWindowSurface);
+
+}
+size_t GetIndex(SDL_Surface * noptrSurface, int r , int c){
+
+	return r * noptrSurface->w + c;
+}
+
